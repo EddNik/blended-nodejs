ae974a0431184037ad1092e0304801e7ad504b92
Eduard <nikinna86@gmail.com>
2025-11-29 13:46:17 +0200

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+import { logger } from './middlewares/logger.js';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 
@@ -9,6 +13,9 @@ const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
 app.use(cors());
+app.use(notFoundHandler);
+app.use(errorHandler);
+app.use(logger);
 
 await connectMongoDB();
 app.listen(PORT, () => {
